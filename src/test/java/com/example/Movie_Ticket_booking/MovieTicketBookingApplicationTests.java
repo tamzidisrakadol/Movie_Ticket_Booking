d7c381bce47acd67336cd0b78f0f076a93dc43b1
Tamzid Israk <tamzidisrak12@gmail.com>
2023-11-19 23:15:19 +0600

    Movie Ticket Booking System

---
+++ src/test/java/com/example/Movie_Ticket_booking/MovieTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Movie_Ticket_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
